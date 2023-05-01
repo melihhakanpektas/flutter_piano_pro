f8ce9030a803a95f4bc301317f333e91ae4bfe40
MelihHakanPektas <melihhakanpektas@gmail.com>
2023-05-02 02:03:28 +0300

    more fields added

---
+++ example/lib/main.dart
@@ -0,0 +1,151 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_piano_pro/flutter_piano_pro.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatefulWidget {
+  const MainApp({super.key});
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  double noteCount = 7;
+  double width = 250;
+  double height = 250;
+  bool expand = true;
+  NoteNames firstNote = NoteNames.noteDo;
+
+  bool showNames = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(colorSchemeSeed: Colors.brown),
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Flutter Piano Pro')),
+        body: ListView(
+          children: [
+            ListTile(
+              title: const Text('First Note'),
+              trailing: SizedBox(
+                width: 250,
+                child: DropdownButton(
+                    value: firstNote,
+                    items: List.generate(NoteNames.values.length, (index) {
+                      return DropdownMenuItem(
+                          value: NoteNames.values[index],
+                          child: Text(NoteNames.values[index]
+                              .toString()
+                              .split('.note')[1]));
+                    }),
+                    onChanged: (value) => setState(() => firstNote = value!)),
+              ),
+            ),
+            const Divider(
+              height: 0,
+            ),
+            ListTile(
+              title: Column(
+                children: [
+                  const Text(
+                    'Note Count',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Text(noteCount.round().toInt().toString()),
+                ],
+              ),
+              trailing: SizedBox(
+                width: 250,
+                child: Slider(
+                  divisions: 51,
+                  max: 52,
+                  min: 1,
+                  value: noteCount,
+                  onChanged: (value) => setState(() => noteCount = value),
+                ),
+              ),
+            ),
+            const Divider(
+              height: 0,
+            ),
+            ListTile(
+              title: Column(
+                children: [
+                  const Text(
+                    'Height',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Text(height.round().toInt().toString()),
+                ],
+              ),
+              trailing: SizedBox(
+                width: 250,
+                child: Slider(
+                  divisions: 27,
+                  max: 300,
+                  min: 150,
+                  value: height,
+                  onChanged: (value) => setState(() => height = value),
+                ),
+              ),
+            ),
+            const Divider(
+              height: 0,
+            ),
+            ListTile(
+              enabled: !expand,
+              title: Column(
+                children: [
+                  const Text(
+                    'Width',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Text(width.round().toInt().toString()),
+                ],
+              ),
+              trailing: SizedBox(
+                width: 250,
+                child: Slider(
+                  divisions: 50,
+                  max: 5000,
+                  min: 100,
+                  value: width,
+                  onChanged: (value) => setState(() => width = value),
+                ),
+              ),
+            ),
+            const Divider(
+              height: 0,
+            ),
+            CheckboxListTile(
+                title: const Text('Expand horizontal to parent'),
+                value: expand,
+                onChanged: (value) => setState(() => expand = value!)),
+            const Divider(
+              height: 0,
+            ),
+            CheckboxListTile(
+                title: const Text('Show Names'),
+                value: showNames,
+                onChanged: (value) => setState(() => showNames = value!)),
+            const SizedBox(
+              height: 20,
+            ),
+            PianoPro.pianoWidget(
+                showNames: showNames,
+                expandHorizontal: expand,
+                totalWidth: width,
+                firstNote: firstNote,
+                buttonHeight: height,
+                buttonCount: noteCount.round().toInt()),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/flutter_piano_pro.dart
+++ lib/flutter_piano_pro.dart
@@ -1,7 +1,125 @@
 library flutter_piano_pro;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+enum NoteNames { noteDo, noteRe, noteMi, noteFa, noteSol, noteLa, noteSi }
+
+class PianoPro {
+  static Widget pianoWidget(
+      {required int buttonCount,
+      double? totalWidth,
+      bool expandHorizontal = true,
+      double buttonHeight = 250,
+      bool showNames = true,
+      NoteNames firstNote = NoteNames.noteDo}) {
+    List<String> noteNames = [
+      for (var note in NoteNames.values) note.toString().split('.note')[1]
+    ];
+    List<NoteNames> noBlackOnTheLeft = [NoteNames.noteDo, NoteNames.noteFa];
+    return LayoutBuilder(builder: (context, constraits) {
+      final maxWidth = expandHorizontal
+          ? constraits.maxWidth
+          : totalWidth ?? constraits.maxWidth;
+      final whiteButtonWidth = maxWidth / buttonCount;
+      final blackButtonWidth = whiteButtonWidth / 2;
+      var whiteButtonHeight = buttonHeight;
+      if (constraits.maxHeight < buttonHeight) {
+        whiteButtonHeight = constraits.maxHeight;
+      }
+      final blackButtonHeight = whiteButtonHeight / 1.85;
+      return SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Stack(
+          children: [
+            Row(children: [
+              ...List.generate(
+                buttonCount,
+                (i) {
+                  var index = i + NoteNames.values.indexOf(firstNote);
+                  String noteName = noteNames[index % 7];
+                  return Container(
+                    margin: EdgeInsets.all(whiteButtonWidth / 100),
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 1, vertical: 8),
+                    width: whiteButtonWidth - ((whiteButtonWidth / 100) * 2),
+                    height: whiteButtonHeight,
+                    color: Colors.grey,
+                    child: Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Text(
+                        showNames ? noteName : '',
+                        softWrap: false,
+                        overflow: TextOverflow.visible,
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: whiteButtonWidth / 3),
+                      ),
+                    ),
+                  );
+                },
+              )
+            ]),
+            Row(
+              children: [
+                ...List.generate(
+                  buttonCount,
+                  (i) {
+                    final index = i + NoteNames.values.indexOf(firstNote);
+                    return !noBlackOnTheLeft
+                                .contains(NoteNames.values[(index % 7)]) &&
+                            i != 0
+                        ? Row(
+                            children: [
+                              Container(
+                                margin: const EdgeInsets.only(top: 1),
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 8),
+                                width: blackButtonWidth,
+                                height: blackButtonHeight,
+                                decoration: BoxDecoration(
+                                    color: Colors.black,
+                                    borderRadius: BorderRadius.circular(
+                                        blackButtonWidth / 20)),
+                                child: Align(
+                                  alignment: Alignment.bottomCenter,
+                                  child: Text(
+                                    showNames
+                                        ? "${noteNames[(index % 7) - 1]}♯\n${noteNames[(index % 7)]}♭"
+                                        : '',
+                                    textAlign: TextAlign.center,
+                                    softWrap: false,
+                                    overflow: TextOverflow.visible,
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: blackButtonWidth / 4),
+                                  ),
+                                ),
+                              ),
+                              !(i == buttonCount - 1)
+                                  ? SizedBox(
+                                      width:
+                                          whiteButtonWidth - (blackButtonWidth),
+                                    )
+                                  : const SizedBox(),
+                            ],
+                          )
+                        : i == 0
+                            ? SizedBox(
+                                width:
+                                    whiteButtonWidth - (blackButtonWidth / 2),
+                              )
+                            : SizedBox(
+                                width: whiteButtonWidth,
+                              );
+                  },
+                )
+              ],
+            )
+          ],
+        ),
+      );
+    });
+  }
 }
